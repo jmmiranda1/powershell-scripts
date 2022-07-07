46c29c40c04d9de32da2b34de828e2e886e17919
Juan Miranda <j.m.miranda@outlook.com>
2022-07-06 17:08:03 -0700

    palindrome with PowerShell

---
+++ InterviewQuestions/Get-Palindrome.ps1
@@ -0,0 +1,15 @@
+# Write a function that checks if a string is a palindrome
+
+function Get-Palindrome {
+    param (
+        $Word
+    )
+    
+    $w = $Word.ToCharArray()
+    [array]::Reverse($w)
+    $x = -join($w)
+
+    "$Word reversed is $x"
+
+    $Word -eq $x
+}
